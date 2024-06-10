762f403a143e715830f5ca2b0a2a54104f6654d9
Florencia Luz Duarte <florluzduarte@gmail.com>
2024-06-09 22:10:19 -0300

    feat: add Footer component and globals styles file

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -1,7 +1,26 @@
+import Styled from "@emotion/styled";
+import { fontSizes, colors, fontWeights } from "../../../styles";
+
+const StyledFooter = Styled("footer")`
+  max-width: 26rem;
+  text-align: center;
+  font-size: ${fontSizes.small};
+`;
+
+const StyledAnchor = Styled("a")`
+  text-decoration: none;
+  font-weight: ${fontWeights.bold};
+  color: ${colors.white};
+
+  &:hover {
+    color: ${colors.main};
+  }
+`;
+
 export const Footer = () => {
   return (
-    <>
-      <p>Footer</p>
-    </>
+    <StyledFooter>
+      <span>Made with React, Vite and love by <StyledAnchor href="https://github.com/florluzduarte" target="_blank">FlorLuzDuarte</StyledAnchor></span>
+    </StyledFooter>
   )
 };
---
+++ src/styles/globals.styles.ts
@@ -0,0 +1,31 @@
+export const fontSizes = {
+  small: '1.4rem',
+  normal: '1.6rem',
+  big: '2rem',
+};
+
+export const fontWeights = {
+  regular: '400',
+  bold: '700',
+};
+
+export const colors = {
+  main: '#FFE81F',
+  mainDark: '#CFBD23',
+  mainLight: '#EBDC59',
+  black: '#14151D',
+  gray900: '#14151D',
+  gray700: '#14151D',
+  gray500: '#787771',
+  gray300: '#908E84',
+  gray100: '#B2B1AD',
+  white: '#F3F3F7'
+};
+
+export const gaps = {
+  xs: '0.8rem',
+  sm: '1.6rem',
+  md: '2.4rem',
+  lg: '3.6rem',
+  xl: '4.8rem',
+};
